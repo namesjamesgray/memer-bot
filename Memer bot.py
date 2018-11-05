cfb26ec9e4e8769c7fa5f4af750b6213d2a7f8fb
namesjamesgray <44736437+namesjamesgray@users.noreply.github.com>
2018-11-05 12:51:20 -0500

    Update Memer bot.py

--- Memer bot.py
+++ Memer bot.py
@@ -1,1 +1,38 @@
 
+import discord
+from discord.ext import commands
+import asyncio
+
+bot=commands.Bot(command_prefix='m!')
+
+@bot.event
+async def on_ready():
+	print('The bot is ready!')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('----------------------------')
+
+@bot.command()
+async def help():
+	await bot.say(''')
+@bot.command()
+async def hi():
+	await bot.say('hello')
+
+@bot.command()
+async def you want some():
+	await bot.say('no u')
+
+@bot.command()
+async def meme():
+	await bot.say('SKINNY PENIS')
+	@bot.command()
+async def no u():
+	await bot.say('listen your gay')	
+	@bot.command()
+async def help():
+	await bot.say('owner:safey is gay
+discordID 5107
+commands m!
+hi meme you want some no u ')	
+bot.run('NTAzNDQyMTI3NjI0MjA4Mzk1.Dq26xg.j7_57wgIbB3Z8nPtyRwWS1_Kv3A')
